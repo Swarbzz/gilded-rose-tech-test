e0df305f518c51d586693ac075c065519012e212
George Swarbrick <glswarbrick@hotmail.co.uk>
2020-04-21 10:59:16 +0100

    added some extra tests that pass

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -8,6 +8,17 @@ describe GildedRose do
       GildedRose.new(items).update_quality()
       expect(items[0].name).to eq "a gilded rose"
     end
+
+    it 'changes quality' do
+      items = [Item.new("a gilded rose", 1, 5)]
+      expect{ GildedRose.new(items).update_quality() }.to change{ items[0].quality }
+    end
+
+    it 'degrades faster when the sell-by date has past, at least twice as fast you might say' do
+      items = [Item.new("a gilded rose", -5, 10)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq(8)
+    end
   end
 
 end
