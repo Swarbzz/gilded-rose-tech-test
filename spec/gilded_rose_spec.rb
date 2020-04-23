6a0e8c2826dbe564038b6072fabc4c97a0c32180
George Swarbrick <glswarbrick@hotmail.co.uk>
2020-04-23 11:43:30 +0100

    added tests for backstage pass and context groups

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -34,44 +34,82 @@ describe GildedRose do
       expect(items[0].quality).to eq 0
     end
 
-    it 'aged brie does not degrade' do
-      items = [Item.new("Aged Brie", 2, 0)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].quality).to eq 1
-    end
-    #DD TEST FOR QUALITY INCREASING BY TWO AFTER SELL_IN FOR BRIE
+    context 'when item is Aged Brie' do
 
-    it 'Aged brie increases in quality over time' do
-      items = [Item.new("Aged Brie", 2, 6)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].quality).to be > 6
-    end
-
-    it 'Aged Brie increase in quality by 1 when sell_in is still valid' do
-      items = [Item.new("Aged Brie", 2, 6)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].quality).to be 7
-    end
-
-    it 'An items quality cannot be above 50' do
-      items = [Item.new("Aged Brie", 2, 48)]
-      store = GildedRose.new(items)
-      50.times do
-        store.update_quality()
+      it 'aged brie increases quality by 2 when sell_in has gone below 0' do
+        items = [Item.new("Aged Brie", -1, 8)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to eq 10
       end
-      expect(items[0].quality).to eq 50
+
+      it 'aged brie does not degrade' do
+        items = [Item.new("Aged Brie", 2, 0)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to eq 1
+      end
+
+      it 'Aged brie increases in quality over time' do
+        items = [Item.new("Aged Brie", 2, 6)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to be > 6
+      end
+
+      it 'Aged Brie increase in quality by 1 when sell_in is still valid' do
+        items = [Item.new("Aged Brie", 2, 6)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to be 7
+      end
+
+      it 'An items quality cannot be above 50' do
+        items = [Item.new("Aged Brie", 2, 48)]
+        store = GildedRose.new(items)
+        50.times do
+          store.update_quality()
+        end
+        expect(items[0].quality).to eq 50
+      end
     end
 
-    it 'Sulfuras never changes its quality' do
-      items = [Item.new("Sulfuras, Hand of Ragnaros", 0, 70)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].quality).to be 70
+    context 'when item is Sulfuras' do
+
+      it 'Sulfuras never changes its quality' do
+        items = [Item.new("Sulfuras, Hand of Ragnaros", 0, 70)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to be 70
+      end
+
+      it 'Sulfuras never changes its sell_in value' do
+        items = [Item.new("Sulfuras, Hand of Ragnaros", 5, 70)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].sell_in).to be 5
+      end
     end
 
-    it 'Sulfuras never changes its sell_in value' do
-      items = [Item.new("Sulfuras, Hand of Ragnaros", 5, 70)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].sell_in).to be 5
+    context 'when item is backstage pass' do
+
+      it 'increases quality by 1 when there are 11 days or more left' do
+        items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 15, 45)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to be 46
+      end
+
+      it 'increases in qulaity by 2 when there are 10 to 6 days left' do
+        items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 9, 45)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to be 47
+      end
+
+      it 'increases in quality by 3 when there are less that 6 days left' do
+        items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 5, 45)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to be 48
+      end
+
+      it 'decreases to quality 0 when the sell_in day is 0' do
+        items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 0, 45)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to be 0
+      end
     end
   end
 
